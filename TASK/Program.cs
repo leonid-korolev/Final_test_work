9bd68c648acea0269d1ccb201cf084a97017d8eb
«Leonid <leonkor64@mail.ru>
2022-06-18 18:27:12 +0300

    add TASK

---
+++ TASK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+Console.Clear();
+
+string[] Array =  { "hello", "2", "world", ":-)"};
+string[] newArray = createNewArray(Array, GetCount(Array));
+
+Write($"[ {String.Join(", ",Array)} ] -> ");
+WriteLine("[ {0} ]", String.Join(", ", newArray));
+WriteLine();
+
+int GetCount(string[] Array)
+{
+	int k = 0;
+	for(int i = 0; i < Array.Length; i++)
+	{
+  		if (Array[i].Length <= 3)
+  		k = k +1;
+  	}
+	return k;
+}
+
+string[] createNewArray(string[] Array, int k)
+{
+  	string[] createArray = new string[k];
+	int j = 0;
+	for(int i = 0; i < Array.Length; i++)
+	{
+		if (Array[i].Length <= 3)
+		{
+			createArray[j] = Array[i];
+			j= j + 1;
+		}
+	}
+	return createArray;
+}
